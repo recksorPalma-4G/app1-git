f515686229111e3b82a8d078525305474311bed4
andrea vazquez dominguez <candy1324515@gmail.com>
2019-03-21 08:46:47 -0600

    3

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
  $(document).ready(function (e){
-	 documen.addEventListener ("deviceready",function(){
+	 document.addEventListener ("deviceready",function(){
 		 $('#disp table td').eq(3).text(device.model);
 		 $('#disp table td').eq(5).text(device.cordova);
 		 $('#disp table td').eq(7).text(device.platform);
